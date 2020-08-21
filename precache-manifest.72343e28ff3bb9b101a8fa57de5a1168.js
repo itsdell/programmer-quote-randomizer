098e1fbccf2e83c787f0adf190a7d8ac7e8ad5f0
Janno Tabamo <janno@hyperstacksinc.com>
2020-08-21 18:34:35 +0800

    Updates

---
+++ precache-manifest.72343e28ff3bb9b101a8fa57de5a1168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545ea89e3ec167f8e4f76feef5f5b70e",
+    "url": "/programmer-quote-randomizer/index.html"
+  },
+  {
+    "revision": "bedfe397b12bcdee4c84",
+    "url": "/programmer-quote-randomizer/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "2f3daeb8acc9250c29b3",
+    "url": "/programmer-quote-randomizer/static/css/main.df26ecd5.chunk.css"
+  },
+  {
+    "revision": "bedfe397b12bcdee4c84",
+    "url": "/programmer-quote-randomizer/static/js/2.72cbe948.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/programmer-quote-randomizer/static/js/2.72cbe948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3daeb8acc9250c29b3",
+    "url": "/programmer-quote-randomizer/static/js/main.b1e58104.chunk.js"
+  },
+  {
+    "revision": "c1da6e87bdf653224c9b",
+    "url": "/programmer-quote-randomizer/static/js/runtime-main.e16e90d1.js"
+  }
+]);
